5a090ec607f74d5a6211087a227ffb90622a19a1
Rodrigo Baldo Martins <rodirgobaldone@gmail.com>
2025-02-19 22:03:34 -0300

    Escolhendo a opção
    Agora o usuário pode escolher se confirma ou altere o tipo da sua loja

--- Sabor-express/tipo_loja.py
+++ Sabor-express/tipo_loja.py
@@ -6,3 +6,6 @@ print(f'Sua loja é um {tipo_loja}');
 tipo = ' o tipo da sua loja \n';
 print(f'1 - Confirme {tipo}');
 print(f'2  - Altere {tipo}');
+escolha = 'Escolha a opção:\n';
+confirm = input(escolha);
+print(f'Você escolheu a opção {confirm}');

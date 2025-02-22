25ef52d4cae5e63dbf57654be5ef8ca8f16e8909
Rodrigo Baldo Martins <rodirgobaldone@gmail.com>
2025-02-22 18:39:26 -0300

    Criando a resposta para o  menu principal
    Muito bom quando escolho uma opção e me leva para a tela que eu desejei.

--- Sabor-express/newmenu.py
+++ Sabor-express/newmenu.py
@@ -6,5 +6,19 @@ op2 = f'2 - Listar {loja}';
 op3 = f'3 - Ativar {loja}';
 op4 = f'4 - Sair do {sabor}';
 def menu(): print({sabor}, {op1}, {op2}, {op3}, {op4}, sep='\n');
-print('Como fazer a função do menu no Python!');
 menu();
+digite = 'Digite uma opção: ';
+# def escolha_opcao():    escolha = int(input(digite)); print(f'Você escolheu a opção {escolha}');  
+# escolha_opcao();
+
+escolha = int(input({digite}));
+print(f'Você escolheu a opção {escolha}')
+
+aqui = 'Você está em ';
+outra = 'Escolha outra opção';
+
+if escolha ==1: print({aqui}, {op1})
+elif escolha ==2: print({aqui}, {op2})
+elif escolha ==4: print({aqui}, {op4})
+elif escolha ==3: print({aqui}, {op3})
+else: print('Escolha outra opção')

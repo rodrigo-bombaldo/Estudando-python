fda09e3e4d4618652ed6fc39c393f801358b8f11
Rodrigo Baldo Martins <rodirgobaldone@gmail.com>
2025-02-25 21:50:41 -0300

    Praticando Python
    Estou começando a fazer os exerc´cios do curso Praticando funções da Alura

---
+++ Praticando-funções/ex01.py
@@ -0,0 +1,16 @@
+ # Fazer uma função que pega a data de nascimento do aluno da professora Julia e calcula sua diade.
+# Esse trecho do código é para criar a mensagem de boas-vindas para a nova professora da turma!
+hello = 'Seja bem-vindo à turma da professora '
+nome = 'Qual é o nome do seu professora?'
+# pro = input({nome})
+pro = 'Julia'
+print({hello}, {pro})
+
+# Agora vamos para o exercíco 
+
+aluno = 'Luana'
+atual = 2025
+nasc = 2000
+# idade = 125-100
+idade = atual - nasc 
+print(f'{aluno} tem {idade} anos')
